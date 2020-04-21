bc4f71e54b984bc034c0fa50ca475af2eaa6aea9
Roman Kutashenko <kutashenko@gmail.com>
2020-04-21 10:25:47 +0300

    Include configs

---
+++ scripts/config/config-all.sh
@@ -0,0 +1,16 @@
+# Common
+export CFG_CLEAN="on"
+export CFG_BUILD_BREAKPAD="on"
+export CFG_BUILD_QXMPP="on"
+export CFG_BUILD_IOTKIT="on"
+export CFG_BUILD_VS_SDK_CPP="on"
+export CFG_BUILD_VS_CRYPTO="on"
+export CFG_QT_SDK_DIR=/opt/Qt/5.12.6
+
+# macOS
+export CFG_BUILD_SPARKLE="on"
+
+# Android
+export CFG_BUILD_ANDROID_CURL_SSL="on"
+export CFG_ANDROID_PLATFORM="android-24"
+export CFG_ANDROID_NDK=/opt/android/ndk
