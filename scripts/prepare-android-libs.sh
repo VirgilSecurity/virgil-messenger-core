79451aea386202cca59be34496029bee7d5983b5
lantaris <edik.ponomarenko@mail.ru>
2020-08-07 14:20:58 +0300

    Static lib for openssl

--- scripts/prepare-android-libs.sh
+++ scripts/prepare-android-libs.sh
@@ -8,7 +8,7 @@ source ${SCRIPT_FOLDER}/ish/common.sh
 #
 BUILD_DIR="${PROJECT_DIR}/tmp/"
 LIBS_REPO="https://github.com/VirgilSecurity/openssl-curl-android.git"
-LIBS_BRANCH="feature/build-debug"
+LIBS_BRANCH="feature/static-lib"
 export INSTALL_DIR_BASE="${PROJECT_DIR}/prebuilt"
 
 #***************************************************************************************
